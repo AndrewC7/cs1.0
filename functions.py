855d8b7bbec8fbac6e2301d8ff4f8327b4a40add
Andrew C <a.cox@me.com>
2020-09-17 01:10:46 -0700

    Add functions.py file

---
+++ functions.py
@@ -0,0 +1,27 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result2 = area(7, 7)
+result3 = area(234, 567)
+
+def subtract(num1, num2)
+    result = num1 - num2
+    return result
+
+result4 = subtract(999, 888)
+result5 = subtract(10, 20)
+print(result4)
+print(result5)
+
+def add(num1, num2)
+    result = num1 + num2
+    return result
+
+result6 = add(50, 50)
+result7 = add(20, 700)
+print(result6)
+print(result7)
