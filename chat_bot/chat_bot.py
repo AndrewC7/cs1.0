df6fe0f5cea330c3cf55ed6c1915a7c0fafa99b3
Andrew C <a.cox@me.com>
2020-10-06 10:48:32 -0700

    Add comments

--- chat_bot/chat_bot.py
+++ chat_bot/chat_bot.py
@@ -1,12 +1,13 @@
 from random import choice
 
-# simple chat bot
+# simple hotdog a sandwich? bot
 def get_bot_response(user_response):
 
   # list of bot responses
   bot_response_yes = ["If you say so", "A hot dog is definitely not a sandwich ", "Whats next? You're going to tell me tacos are sandwiches too?"]
   bot_response_no = ["If you say so", "A hot dog is definitely a sandwich", "Finally, someone who gets it"]
-
+  
+  # return random response from list of bot responses
   if user_response == "yes":
     return choice(bot_response_yes)
   elif user_response == "no":
@@ -14,11 +15,15 @@ def get_bot_response(user_response):
   else:
     return "You must pick a side..."
 
+
+
+
+
 print("Welcome to the \'Is a Hot Dog a Sandwich?\' Bot")
 print("There are arguments to be made for both sides")
 
 user_response = ""
-
+# prompt user to answer question
 while True:
   user_response = input("So, is a hot dog a sandwich?: ")
   
