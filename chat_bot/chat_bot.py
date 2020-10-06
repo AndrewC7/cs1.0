673cb2e9d7a8819eaea73e1cb02b13567d1acd19
Andrew C <a.cox@me.com>
2020-10-06 10:42:14 -0700

    Initial commit of chat_bot

---
+++ chat_bot/chat_bot.py
@@ -0,0 +1,31 @@
+from random import choice
+
+# simple chat bot
+def get_bot_response(user_response):
+
+  # list of bot responses
+  bot_response_yes = ["If you say so", "A hot dog is definitely not a sandwich ", "Whats next? You're going to tell me tacos are sandwiches too?"]
+  bot_response_no = ["If you say so", "A hot dog is definitely a sandwich", "Finally, someone who gets it"]
+
+  if user_response == "yes":
+    return choice(bot_response_yes)
+  elif user_response == "no":
+    return choice(bot_response_no)
+  else:
+    return "You must pick a side..."
+
+print("Welcome to the \'Is a Hot Dog a Sandwich?\' Bot")
+print("There are arguments to be made for both sides")
+
+user_response = ""
+
+while True:
+  user_response = input("So, is a hot dog a sandwich?: ")
+  
+  # break out when user types done
+  if user_response == 'done':
+    break
+
+  
+  bot_response = get_bot_response(user_response)
+  print(bot_response)
